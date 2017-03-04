2b53693c19936fbca0230b33bed57cdbb956fa7b
Antonov Anton <antantval@gmail.com>
2017-03-05 00:27:10 +0300

    Add navigation and mock pages

--- client/app.js
+++ client/app.js
@@ -2,13 +2,26 @@ import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
+import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
 import configureStore from './store'
 
 import App from 'containers/App'
+import Home from 'containers/Home'
+import Settings from 'containers/Settings'
+import Audio from 'containers/Audio'
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <App/>
+    <Router
+      history={browserHistory}
+    >
+      <Route path='/' component={App}>
+        <IndexRedirect to='/home'/>
+        <Route path='home' component={Home}/>
+        <Route path='settings' component={Settings}/>
+        <Route path='audio' component={Audio}/>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 )
---
+++ client/containers/Audio/index.js
@@ -0,0 +1,11 @@
+import React, {PureComponent} from 'react'
+
+export class Audio extends PureComponent {
+  render() {
+    return (
+      <div>Audios</div>
+    )
+  }
+}
+
+export default Audio
