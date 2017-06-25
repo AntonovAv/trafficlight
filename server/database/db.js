05f27c2d8176d0ea46ef355f41916d09e95f74b9
Antonov Anton <antantval@gmail.com>
2017-06-26 02:05:39 +0300

    Stop process if db connection fails

--- server/database/db.js
+++ server/database/db.js
@@ -22,6 +22,7 @@ mongoose.connection.on('connected', function() {
 // If the connection throws an error
 mongoose.connection.on('error', function(err) {
   console.log('Mongoose default connection error: ' + err)
+  throw err
 })
 
 // When the connection is disconnected
