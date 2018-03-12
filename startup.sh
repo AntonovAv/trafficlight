400603ae4d9f41cb892d28102bf8e7e2c1222e60
antonov <antantval@gmail.com>
2018-03-12 03:17:17 +0300

    Add waiting while mongo starts before run server

--- startup.sh
+++ startup.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 cd $(cd "$(dirname "$0")" && pwd)
-npm run start:prod > ./log.txt 2>&1
+COUNTER=0
+systemctl status mongodb | grep -q "inactive"
+while [[  $? -ne 0 && $COUNTER -lt 60 ]] ; do
+    sleep 2
+    let COUNTER+=2
+    echo "Waiting for mongo to initialize... ($COUNTER seconds so far)" >> ./log.txt
+    systemctl status mongodb | grep -q "inactive"
+done
+npm run start:prod >> ./log.txt 2>&1
