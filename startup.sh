354c0571325cb8a73e4e7f7305a7128dcb296981
antonov <antantval@gmail.com>
2018-03-12 22:38:47 +0300

    startup.sh fixes

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 cd $(cd "$(dirname "$0")" && pwd)
 COUNTER=0
 systemctl status mongodb | grep -q "inactive"
-while [[  $? -ne 0 && $COUNTER -lt 60 ]] ; do
+while [[ $? -eq 0 && $COUNTER -lt 60 ]] ; do
     sleep 2
     let COUNTER+=2
     echo "Waiting for mongo to initialize... ($COUNTER seconds so far)" >> ./log.txt
